f1accebb101b1f9d95f20e25d4d16b4f6f772998
Hannah.G.May <Hannah.G.May@target.com>
2018-01-10 13:20:33 -0600

    change shelf functionality in app.js changed some typos in bookshelf was not calling the Book functionality correct

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -11,7 +11,7 @@ class BookShelf extends Component{
         // create the shelf types with filter 
         const currentlyReading = books.filter(book => book.shelf === "currentlyReading");
         const wantToRead = books.filter(book => book.shelf === "wantToRead");
-        const read = books.filter(book => books.shelf === "read");
+        const read = books.filter(book => book.shelf === "read");
 
         return(
             // header
@@ -20,41 +20,45 @@ class BookShelf extends Component{
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-            <div>
-            {/* currently reading bookshelf */}
-            <div className="bookshelf">
+                <div>
+                {/* currently reading bookshelf */}
+                <div className="bookshelf">
                     <h2 className="bookshelf-title">Currently Reading</h2>
-                   { currentlyReading.lenght > 0 &&
-                   <book 
+                    {currentlyReading.length > 0 &&
+                   <Book 
                     books={currentlyReading}
                     changeShelf={this.props.changeShelf}
                    />}
             </div>
-             {/* want to read bookshelf */}
+            {/* want to read bookshelf */}
             <div className="bookshelf">
-                    <h2 className="bookshelf-title">Want To Read</h2>
-                   { wantToRead.lenght > 0 &&
-                   <book 
+                <h2 className="bookshelf-title">Want To Read</h2>
+                {wantToRead.length > 0 &&
+                   <Book 
                     books={wantToRead}
                     changeShelf={this.props.changeShelf}
                    />}
             </div>
              {/* read bookshelf */}
-            <div className="bookshelf">
-                    <h2 className="bookshelf-title">Read</h2>
-                   { read.lenght > 0 &&
-                   <book 
-                    books={read}
-                    changeShelf={this.props.changeShelf}
-                   />}
-            </div>
+             <div className="bookshelf">
+             <h2 className="bookshelf-title">Read</h2>
+             {read.length > 0 &&
+                <Book 
+                 books={read}
+                 changeShelf={this.props.changeShelf}
+                />}
+         </div>
             </div>
             </div>
             {/* search part brought in */}
+
             </div>
+
 
         )
 
 
     }
 }
+
+export default BookShelf;
